6da9ae2dfe8a9b994660a45d0070c51db47d4087
TUMIT_HASAN <tumithasan1@gmail.com>
2025-07-02 15:46:31 +0600

    implement Google Analytics v4 on my project

--- src/components/data/projects.js
+++ src/components/data/projects.js
@@ -27,10 +27,21 @@ const projects = [
         image: "https://i.ibb.co/jP63W2wx/Zap-Shift.jpg",
         techStack: ["React, Firebase Authentication, MongoDB, Express.js, Tailwind CSS"],
         description: "A parcel delivery web app where users can create delivery requests, track parcels, and manage logistics with secure authentication and real-time updates.",
-        liveLink: "https://roommate-finder-application.web.app",
+        liveLink: "https://zap-shift-application.web.app",
         githubClient: "https://github.com/tumit-h-r-75/zap-shift-client",
         challenges: "Integrating Firebase auth with MongoDB, implementing secure CRUD operations, and dynamic parcel tracking ID generation.",
         futurePlans: "Add real-time delivery tracking, delivery status notifications, and rider-side dashboard for pickups and deliveries.",
+    },
+    {
+        id: 4,
+        name: "Volunteer Hub",
+        image: "https://i.ibb.co/MDSKRL65/Screenshot-2025-07-01-083350.png",
+        techStack: ["React", "Firebase Authentication", "MongoDB", "Express.js", "Tailwind CSS"],
+        description: "A volunteer coordination platform where users can post and manage volunteer tasks, register to participate in events, and view impact analytics with secure authentication and dynamic dashboards.",
+        liveLink: "https://volunteer-hub-app.web.app",
+        githubClient: "https://github.com/tumit-h-r-75/assigment-11-client",
+        challenges: "Synchronizing Firebase authentication with MongoDB user roles, managing real-time task updates, and ensuring secure access to sensitive volunteer data.",
+        futurePlans: "Introduce event calendar integration, volunteer contribution certificates, and admin analytics dashboard for engagement tracking."
     }
 ];
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import './index.css';
 import { RouterProvider } from 'react-router';
 import router from './router/router';
 import NavigateProvider from './context/NavigateProvider';
+import ReactGA from 'react-ga4';
+
+// ✅ Initialize your Google Analytics
+ReactGA.initialize("G-9QH68429Q0"); // <-- তোমার Measurement ID বসাও
+ReactGA.send("pageview"); // <-- Home page visitor count
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
